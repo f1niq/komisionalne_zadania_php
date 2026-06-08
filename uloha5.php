350b708e1deca31aef163530b075bef666f794f1
f1niq <filip@koli.sk>
2026-06-08 18:13:36 +0200

    uloha5 hotovo

--- uloha5.php
+++ uloha5.php
@@ -27,6 +27,57 @@ Požiadavky:
     <pre><?php echo htmlspecialchars(file_get_contents(__FILE__)); ?></pre>
 
     <!-- Sem napíš svoje riešenie. -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="priklad5.php" method="post">
+        <label>Meno</label>
+        <input type="text" name="meno" placeholder="zadaj meno">
+        <br>
+        <label>Email</label>
+        <input type="email" name="email" placeholder="zadaj mail">
+        <button type="submit">Odosli</button>
 
+    </form>
+</body>
+</html>
+</body>
+</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $spojenie = mysqli_connect('localhost', 'root', 'root', 'skola');
+
+if(!$spojenie){
+    echo "nepripojene";
+} else {
+    echo "uspesne pripojene";
+    echo "<br>";
+}
+if(!empty($_POST['meno']) && !empty($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+    $email = $_POST['email'];
+    $meno = $_POST['meno'];
+$stmt = mysqli_prepare($spojenie,"INSERT INTO ziaci (meno,email) VALUES (?,?)");
+mysqli_stmt_bind_param($stmt,"ss",$meno,$email);
+mysqli_stmt_execute($stmt);
+
+echo "ziak pridany";
+}
+
+
+
+
+?>
 </body>
 </html>
